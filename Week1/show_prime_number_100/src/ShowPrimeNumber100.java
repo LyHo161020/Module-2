3522985c9e8b7d99c09de3719da57f4dc62230c8
LyHo161020 <holy161020@gmail.com>
2022-03-31 13:51:26 +0700

    [Bài tập] Hiển thị các số nguyên tố nhỏ hơn 100

---
+++ Week1/show_prime_number_100/src/ShowPrimeNumber100.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ShowPrimeNumber100 {
+    public static void main(String[] args) {
+
+        for (int i = 2 ; i <=100; i++ ) {
+            boolean flag = true;
+           for (int j = 2; j < i ; j++) {
+               if (i % j == 0) {
+                   flag = false;
+               }
+           }
+
+           if (flag) {
+               System.out.printf("%d\t",i);
+           }
+        }
+
+    }
+}
