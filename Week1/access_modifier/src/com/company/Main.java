e66a49f1ac9646d6df9b009a34c08b7a53c60bf2
LyHo161020 <holy161020@gmail.com>
2022-04-06 10:23:52 +0700

    [Bài tập] Access modifier

---
+++ Week1/access_modifier/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Circle circle = new Circle(2.0);
+        System.out.println(circle.getRadius());
+        System.out.printf("%.2f",circle.getArea());
+    }}
