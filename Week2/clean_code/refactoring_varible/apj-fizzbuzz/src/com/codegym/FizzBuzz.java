6b20157889ac45dc178e7514ae0c456aead79afc
LyHo161020 <holy161020@gmail.com>
2022-04-16 21:05:21 +0700

    [Thực hành] Refactoring - tách biến

---
+++ Week2/clean_code/refactoring_varible/apj-fizzbuzz/src/com/codegym/FizzBuzz.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+public class FizzBuzz {
+
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
+    public static String fizzBuzz(int number){
+        if(number % 3 == 0 && number % 5 == 0)
+            return FIZZ_BUZZ;
+
+        if(number % 3 == 0)
+            return FIZZ;
+
+        if(number % 5 == 0)
+            return BUZZ;
+
+        return number + "";
+    }
+}
