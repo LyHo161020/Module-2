17cd8e112b968d4b89eb7cffc14f42e6584e2483
LyHo161020 <holy161020@gmail.com>
2022-04-25 09:09:00 +0700

    [Thực hành] Cài đặt thuật toán sắp xếp nổi bọt

---
+++ Week3/sort_algorithm/bubble_sort/src/com/company/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static int[] bubbleSort(int[] arr){
+        for(int i = 0; i < arr.length - 1;i++){
+            for (int j = 0; j < arr.length - 1 - i; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(bubbleSort(list)));
+    }
+}
